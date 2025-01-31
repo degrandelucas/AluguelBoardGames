a461e9c9b074aea14355c2894af7f4ff0434779c
degrandelucas <lucasdegrande15@gmail.com>
2025-01-31 18:41:19 -0300

    add class para escurecer imagem apos selecionar o jogo

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function alterarStatus(numero){
+    let status = document.getElementById("game-"
+    +numero);
+    status.classList.add("dashboard__item__button--return","dashboard__item__img--rented");
+}
